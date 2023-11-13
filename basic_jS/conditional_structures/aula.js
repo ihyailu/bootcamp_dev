c4a1c7e2a885ffed2d6ebf9377aef5598f460949
ihyailu <81053310+ihyailu@users.noreply.github.com>
2023-11-13 11:09:46 -0300

    exemplos de resto de divisão

--- basic_jS/conditional_structures/aula.js
+++ basic_jS/conditional_structures/aula.js
@@ -1,2 +1,8 @@
-const camisetaPreta = true;
-const camisetaBranca = false;
+// const camisetaPreta = true;
+// const camisetaBranca = false;
+
+const numero = 15;
+
+const numeroPar = (numero % 5) === 0;
+
+console.log(numeroPar);
